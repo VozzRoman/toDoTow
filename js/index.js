95e23043fba18923f5e83abb62482194c499c408
VozzRoman <looper0500@ukr.net>
2022-12-04 12:51:07 +0200

    adding

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@
 const addTask = document.querySelector('.box-red');
 const removeTask = document.querySelector('.box-green');
 const enterTask = document.querySelector('.input-js');
+const todoListContainer = document.querySelector('.todo-list');
 console.log(addTask);
 console.log(removeTask);
 
 
+const LOCAL__KEY = 'localKey';
 
 addTask.addEventListener('click', onAddTask);
 
@@ -16,7 +18,7 @@ function onAddTask() {
 		id: Date.now(),
 		value: enterTask.value,
 	}
-	let data = JSON.parse(localStorage.getItem('localKey'));
+	let data = JSON.parse(localStorage.getItem(LOCAL__KEY));
 	if (!data) {
 		data = [];
 	}
@@ -24,10 +26,18 @@ function onAddTask() {
 	console.log(data);
 	const toStringData = JSON.stringify(data);
 	console.log(toStringData);
-	const localData = localStorage.setItem('localKey', toStringData);
+	const localData = localStorage.setItem(LOCAL__KEY, toStringData);
 	console.log(localData);
-
-
-
-	
+	enterTask.value = '';
+	todoListContainer.insertAdjacentHTML('beforeend', toDoMarkUp(data));
 }
+
+function toDoMarkUp(objecData) {
+	todoListContainer.innerHTML = '';
+	 const elemnts = objecData.map(({ id, value }) => {
+		 return `<li class="" id="${id}">${value}</li>`
+	}).join('');
+	return elemnts;
+}
+
+
